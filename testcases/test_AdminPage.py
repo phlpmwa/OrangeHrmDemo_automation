76174d2bed281484e25d33de804da671b01e8569
Philip Mwangi <mwaphiwa@gmail.com>
2022-11-01 17:48:05 +0300

    sending results to testrail

--- testcases/test_AdminPage.py
+++ testcases/test_AdminPage.py
@@ -2,9 +2,11 @@ import time
 
 from pages.LoginPage import LoginPage
 from testcases.BaseTest import BaseTest
+from pytest_testrail.plugin import  pytestrail
 
 
 class Test_AdminPage(BaseTest):
+    @pytestrail.case('C1', 'C2')
     def test_add_admin_user(self):
         login = LoginPage(self.driver)
         homepage = login.do_login()
